3e32c7b8398a9d5423179b9a76067d614242f43a
Matt Goldman <robotmlg@gmail.com>
2016-03-26 18:45:57 -0400

    More installing

--- brew.sh
+++ brew.sh
@@ -12,14 +12,23 @@ fi
 brew update
 brew upgrade --all
 
+# update bash
+brew install bash
+echo "Adding new bash to allowed shells"
+sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
+chsh -s /usr/local/bin/bash # change user shell to new one
+
 # core
 brew install git
-brew install vim 
+brew install vim --override-system-vi
 brew install tmux
 
 # utils
+brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 brew install stow
 brew install tree
+brew install nmap
 
 # languages
 brew install python3
@@ -27,3 +36,18 @@ brew install python3
 # fun
 brew install sl
 brew install gti
+
+# set up casks
+brew tap caskroom/cask
+
+# apps
+brew cask install dropbox
+brew cask install flux
+brew cask install slimbatterymonitor
+brew cask install bettertouchtool
+brew cask install twitterrific
+brew cask install iterm2
+brew cask install java
+brew cask install firefox
+
+brew cleanup
